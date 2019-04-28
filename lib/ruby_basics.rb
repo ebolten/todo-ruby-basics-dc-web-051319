0caedaebe92456f6b294e8f45bfd5aeaa5ee5a5b
Emily Bolten <emilybolten@gmail.com>
2019-04-28 19:00:34 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,13 +1,13 @@
 def division(num1, num2)
- 
+  return num1/num2
 end
 
 def assign_variable(value)
-  
+  value = "Emily"
 end
 
-def argue
-  
+def argue(phrase = "I am right and you are wrong!")
+  return arg
 end
 
 def greeting
